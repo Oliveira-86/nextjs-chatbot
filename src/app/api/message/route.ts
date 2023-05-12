c7322b84c2247d399cc50d4f19b6ba3b5f482d44
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-05-12 15:43:50 -0300

    implementing real-time response streaming

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,11 +1,15 @@
 import { chatbotPrompt } from '@/helpers/constant/chatbot-prompt'
-import { ChatGPTMessage } from '@/lib/openai-stream'
+import {
+  ChatGPTMessage,
+  OpenAIStreamPayload,
+  OpenAIStream,
+} from '@/lib/openai-stream'
 import { MessageArraySchema } from '@/lib/validator/message'
 
 export async function POST(request: Request) {
-  const { message } = await request.json()
+  const { messages } = await request.json()
 
-  const parsedMessages = MessageArraySchema.parse(message)
+  const parsedMessages = MessageArraySchema.parse(messages)
 
   const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => ({
     role: message.isUserMessage ? 'user' : 'system',
@@ -16,4 +20,20 @@ export async function POST(request: Request) {
     role: 'system',
     content: chatbotPrompt,
   })
+
+  const payload: OpenAIStreamPayload = {
+    model: 'gpt-3.5-turbo',
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  }
+
+  const stream = await OpenAIStream(payload)
+
+  return new Response(stream)
 }
