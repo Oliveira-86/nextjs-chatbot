d9b34486c3d85eb28d5b53cac00189d589ace9f5
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-05-13 16:49:17 -0300

    added gif to readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main className="bg-black w-full h-[100vh]"></main>
+  return (
+    <main className="bg-black w-full h-[100vh] flex items-center justify-center">
+      <p className="text-yellow-50">Web Content</p>
+    </main>
+  )
 }
